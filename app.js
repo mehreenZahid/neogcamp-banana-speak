8c72e52438951512710f8cbbe098601b2ae4a2ad
mereenzahid <93670902+mereenzahid@users.noreply.github.com>
2023-09-06 15:07:42 +0530

    added fetch call

--- app.js
+++ app.js
@@ -2,9 +2,28 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txtarea-input");
 var divOutput = document.querySelector("#output")
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input){
+    return serverURL +"?"+ "text="+ input
+}
+
+function errorHandler(error){
+    console.log("error occured", error)
+    alert("Something went wrong with server! Try again after some time")
+}
 
 function clickEventHandler(){
-    divOutput.innerText = "hjkbbnhujhjbhj" + txtInput.value
+    var inputText = txtInput.value
+
+    fetch(getTranslationURL(inputText))
+     .then(response => response.json())
+     .then(json => {
+        var translatedText = json.contents.translated
+        divOutput.innerText = translatedText
+     })
+     .catch(errorHandler)
+    
 }
 
 btnTranslate.addEventListener("click", clickEventHandler)
